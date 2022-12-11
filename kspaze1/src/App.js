7b63679f0def097d8fe11eb1831cb9b3695c641c
ktyntang <ktyntang@hotmail.com>
2022-12-11 16:08:25 +0800

    Update PageAbout per client copy

--- kspaze1/src/App.js
+++ kspaze1/src/App.js
@@ -1,162 +1,193 @@
-import React, {useState, useEffect, Suspense, useMemo} from 'react';
-import {getSortedUrls,getImgCaptions} from './utils/firebase'
-import { ParallaxProvider } from 'react-scroll-parallax';
-import './App.css';
-import Landing from './components/Landing';
-import NavBar from './components/NavBar';
-import HomeMain from './components/HomeMain';
-import Loading from './components/Loading';
-import placeholder from './assets/placeholderImg.webp'
+import React, { useState, useEffect, Suspense, useMemo } from "react";
+import { getSortedUrls, getImgCaptions } from "./utils/firebase";
+import { ParallaxProvider } from "react-scroll-parallax";
+import "./App.css";
+import Landing from "./components/Landing";
+import NavBar from "./components/NavBar";
+import HomeMain from "./components/HomeMain";
+import Loading from "./components/Loading";
+import placeholder from "./assets/placeholderImg.webp";
 
-
-const SocialBubbles = React.lazy(()=>import('./components/SocialBubbles'));
-const homePromise = import('./components/Home');
-const Home = React.lazy(()=>homePromise)
-const pagesPromise = import('./components/PageRouter');
-const PageRouter = React.lazy(()=>pagesPromise)
+const SocialBubbles = React.lazy(() => import("./components/SocialBubbles"));
+const homePromise = import("./components/Home");
+const Home = React.lazy(() => homePromise);
+const pagesPromise = import("./components/PageRouter");
+const PageRouter = React.lazy(() => pagesPromise);
 
 // FUTURE IMPROVEMENTS
 // srcset 1px imgs for mobile.
 
 function App() {
-  const [enter, setEnter] = useState(false)
-  const [page,setPage] = useState('landing')
-  const [show, setShow] = useState(false);
-  const [landingImgList, setLandingImgList] = useState([])
-  const [homeMainImgList, setHomeMainImgList] = useState([])
-  const [homeEntered, setHomeEntered] = useState(false)
+	const [enter, setEnter] = useState(false);
+	const [page, setPage] = useState("landing");
+	const [show, setShow] = useState(false);
+	const [landingImgList, setLandingImgList] = useState([]);
+	const [homeMainImgList, setHomeMainImgList] = useState([]);
+	const [homeEntered, setHomeEntered] = useState(false);
 
-  useEffect(()=>{
-    getSortedUrls('landing').then(res=>setLandingImgList(res)) 
-    getSortedUrls('homeMain').then(res=>setHomeMainImgList(res))     
-    },[])
+	useEffect(() => {
+		getSortedUrls("landing").then((res) => setLandingImgList(res));
+		getSortedUrls("homeMain").then((res) => setHomeMainImgList(res));
+	}, []);
 
-  const [bannerImgList, setBannerImgList] = useState([])
-  const [aboutImgList, setAboutImgList] = useState([])
-  const [aboutCaptionList, setAboutCaptionList] = useState([])
-  const [featureImgList, setFeatureImgList] = useState([])
-  const [featureCaptionList, setFeatureCaptionList] = useState([])
-  const [NFTImgList, setNFTImgList] = useState([])
-  const [NFTCaptionList, setNFTCaptionList] = useState([])
-  const [projectsImgList, setProjectsImgList] = useState([])
-  const [projectsCaptionList, setProjectsCaptionList] = useState([])
-  const [aboutPageImgList, setAboutPageImgList] = useState([])
-  const [aboutPageCaptionList, setAboutPageCaptionList] = useState([])
-  const [featurePageImgList, setFeaturePageImgList] = useState([])
-  const [featurePageCaptionList, setFeaturePageCaptionList] = useState([])
-  const [NFTPageImgList, setNFTPageImgList] = useState([])
-  const [NFTPageCaptionList, setNFTPageCaptionList] = useState([])
-  const [projectsPageImgList, setProjectsPageImgList] = useState([])
-  const [projectsPageCaptionList, setProjectsPageCaptionList] = useState([])
+	const [bannerImgList, setBannerImgList] = useState([]);
+	const [aboutImgList, setAboutImgList] = useState([]);
+	const [aboutCaptionList, setAboutCaptionList] = useState([]);
+	const [featureImgList, setFeatureImgList] = useState([]);
+	const [featureCaptionList, setFeatureCaptionList] = useState([]);
+	const [NFTImgList, setNFTImgList] = useState([]);
+	const [NFTCaptionList, setNFTCaptionList] = useState([]);
+	const [projectsImgList, setProjectsImgList] = useState([]);
+	const [projectsCaptionList, setProjectsCaptionList] = useState([]);
+	const [aboutPageImgList, setAboutPageImgList] = useState([]);
+	const [aboutPageCaptionList, setAboutPageCaptionList] = useState([]);
+	const [featurePageImgList, setFeaturePageImgList] = useState([]);
+	const [featurePageCaptionList, setFeaturePageCaptionList] = useState([]);
+	const [NFTPageImgList, setNFTPageImgList] = useState([]);
+	const [NFTPageCaptionList, setNFTPageCaptionList] = useState([]);
+	const [projectsPageImgList, setProjectsPageImgList] = useState([]);
+	const [projectsPageCaptionList, setProjectsPageCaptionList] = useState([]);
 
-  
-  useEffect(()=>{
-    if (homeEntered) { 
-      getSortedUrls('homeAbout').then(res=>setAboutImgList(res))
-      getImgCaptions('homeAbout').then(res=>setAboutCaptionList(res))
-      getSortedUrls('homeBanner').then(res=>setBannerImgList(res))
-      getSortedUrls('homeFeature').then(res=>setFeatureImgList(res))
-      getImgCaptions('homeFeature').then(res=>setFeatureCaptionList(res))
-      getSortedUrls('homeNFT').then(res=>setNFTImgList(res))
-      getImgCaptions('homeNFT').then(res=>setNFTCaptionList(res))
-      getSortedUrls('homeProjects').then(res=>setProjectsImgList(res))
-      getImgCaptions('homeProjects').then(res=>setProjectsCaptionList(res))
-      getSortedUrls('pageAbout').then(res=>setAboutPageImgList(res))
-      getImgCaptions('pageAbout').then(res=>setAboutPageCaptionList(res))
-      getSortedUrls('pageFeature').then(res=>setFeaturePageImgList(res))
-      getImgCaptions('pageFeature').then(res=>setFeaturePageCaptionList(res))
-      getSortedUrls('pageNFT').then(res=>setNFTPageImgList(res))
-      getImgCaptions('pageNFT').then(res=>setNFTPageCaptionList(res))
-      getSortedUrls('pageProjects').then(res=>setProjectsPageImgList(res))
-      getImgCaptions('pageProjects').then(res=>setProjectsPageCaptionList(res))
-    }
-  },[homeEntered])
-  
-  const toggleLanding = ()=>{
-    setEnter((enter)=>!enter)
-    setHomeEntered(true)}
+	useEffect(() => {
+		if (homeEntered) {
+			getSortedUrls("homeAbout").then((res) => setAboutImgList(res));
+			getImgCaptions("homeAbout").then((res) => setAboutCaptionList(res));
+			getSortedUrls("homeBanner").then((res) => setBannerImgList(res));
+			getSortedUrls("homeFeature").then((res) => setFeatureImgList(res));
+			getImgCaptions("homeFeature").then((res) =>
+				setFeatureCaptionList(res)
+			);
+			getSortedUrls("homeNFT").then((res) => setNFTImgList(res));
+			getImgCaptions("homeNFT").then((res) => setNFTCaptionList(res));
+			getSortedUrls("homeProjects").then((res) =>
+				setProjectsImgList(res)
+			);
+			getImgCaptions("homeProjects").then((res) =>
+				setProjectsCaptionList(res)
+			);
+			getSortedUrls("pageAbout").then((res) => setAboutPageImgList(res));
+			getImgCaptions("pageAbout").then((res) =>
+				setAboutPageCaptionList(res)
+			);
+			getSortedUrls("pageFeature").then((res) =>
+				setFeaturePageImgList(res)
+			);
+			getImgCaptions("pageFeature").then((res) =>
+				setFeaturePageCaptionList(res)
+			);
+			getSortedUrls("pageNFT").then((res) => setNFTPageImgList(res));
+			getImgCaptions("pageNFT").then((res) => setNFTPageCaptionList(res));
+			getSortedUrls("pageProjects").then((res) =>
+				setProjectsPageImgList(res)
+			);
+			getImgCaptions("pageProjects").then((res) =>
+				setProjectsPageCaptionList(res)
+			);
+		}
+	}, [homeEntered]);
 
- const openModal =(pageID)=>{
-    setShow(true)
-    setPage(pageID)
-    //disable scroll
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    window.onscroll = () => window.scrollTo(0, scrollTop);
-  }
+	const toggleLanding = () => {
+		setEnter((enter) => !enter);
+		setHomeEntered(true);
+	};
 
-  const closePage =() =>{
-    setShow(false);
-    //enable scroll
-    window.onscroll = function() {};
-  }
+	const openModal = (pageID) => {
+		setShow(true);
+		setPage(pageID);
+		//disable scroll
+		let scrollTop =
+			window.pageYOffset || document.documentElement.scrollTop;
+		window.onscroll = () => window.scrollTo(0, scrollTop);
+	};
 
-  function handleScrollSlide() {
-    var reveals = document.querySelectorAll(".js-scroll");
-  
-    for (let i = 0; i < reveals.length; i++) {
-      const windowHeight = window.innerHeight;
-      let elementTop = reveals[i].getBoundingClientRect().top;
-      const elementVisible = 50;
-  
-      if (elementTop < (windowHeight - elementVisible)) {
-        reveals[i].classList.add("scrolled");
-      } else {
-        reveals[i].classList.remove("scrolled");
-      }
-    }
-  }
-  window.addEventListener("scroll", handleScrollSlide);
+	const closePage = () => {
+		setShow(false);
+		//enable scroll
+		window.onscroll = function () {};
+	};
 
-  const homeMainMemo = useMemo(() => <HomeMain homeMainImgList={homeMainImgList} placeholder={placeholder}/>, [homeMainImgList]);
+	function handleScrollSlide() {
+		var reveals = document.querySelectorAll(".js-scroll");
 
-  
-  return (
-    <div className="App">
-      {!enter && <Landing toggleLanding={toggleLanding} landingImgList={landingImgList}/>}
-      {enter &&
-      <div>
-        <ParallaxProvider>
-         {homeMainMemo}
-        </ParallaxProvider>
-        <NavBar toggleLanding={toggleLanding} />
-        <Suspense fallback={<Loading/>}>
-          <Home 
-          openModal={openModal}
-          placeholder={placeholder}
-          bannerImgList={bannerImgList}
-          aboutImgList ={aboutImgList}
-          aboutCaptionList ={aboutCaptionList}
-          featureImgList ={featureImgList}
-          featureCaptionList ={featureCaptionList}
-          NFTImgList ={NFTImgList}
-          NFTCaptionList ={NFTCaptionList}
-          projectsImgList ={projectsImgList}
-          projectsCaptionList ={projectsCaptionList}
-          />
-        </Suspense>
-      </div>}
-      {show && 
-      <Suspense fallback={<Loading/>}>
-        <PageRouter pageID={page} closePage={closePage} placeholder={placeholder}
-        aboutPageImgList={aboutPageImgList}
-        aboutPageCaptionList={aboutPageCaptionList}
-        featurePageImgList={featurePageImgList}
-        featurePageCaptionList={featurePageCaptionList}
-        NFTPageImgList={NFTPageImgList}
-        NFTPageCaptionList={NFTPageCaptionList}
-        projectsPageImgList={projectsPageImgList}
-        projectsPageCaptionList={projectsPageCaptionList}
-        />
-      </Suspense>
-      }
-      <footer>
-        <h1>Get In Touch</h1>
-        <Suspense fallback={<p></p>}>
-          <SocialBubbles/>
-        </Suspense>
-        <p>©2022 Kspaze1. All rights reserved.</p></footer>
-    </div>
-  );
+		for (let i = 0; i < reveals.length; i++) {
+			const windowHeight = window.innerHeight;
+			let elementTop = reveals[i].getBoundingClientRect().top;
+			const elementVisible = 50;
+
+			if (elementTop < windowHeight - elementVisible) {
+				reveals[i].classList.add("scrolled");
+			} else {
+				reveals[i].classList.remove("scrolled");
+			}
+		}
+	}
+	window.addEventListener("scroll", handleScrollSlide);
+
+	const homeMainMemo = useMemo(
+		() => (
+			<HomeMain
+				homeMainImgList={homeMainImgList}
+				placeholder={placeholder}
+			/>
+		),
+		[homeMainImgList]
+	);
+
+	return (
+		<div className="App">
+			{!enter && (
+				<Landing
+					toggleLanding={toggleLanding}
+					landingImgList={landingImgList}
+				/>
+			)}
+			{enter && (
+				<div>
+					<ParallaxProvider>{homeMainMemo}</ParallaxProvider>
+					<NavBar toggleLanding={toggleLanding} />
+					<Suspense fallback={<Loading />}>
+						<Home
+							openModal={openModal}
+							placeholder={placeholder}
+							bannerImgList={bannerImgList}
+							aboutImgList={aboutImgList}
+							aboutCaptionList={aboutCaptionList}
+							featureImgList={featureImgList}
+							featureCaptionList={featureCaptionList}
+							NFTImgList={NFTImgList}
+							NFTCaptionList={NFTCaptionList}
+							projectsImgList={projectsImgList}
+							projectsCaptionList={projectsCaptionList}
+						/>
+					</Suspense>
+				</div>
+			)}
+			{show && (
+				<Suspense fallback={<Loading />}>
+					<PageRouter
+						pageID={page}
+						closePage={closePage}
+						placeholder={placeholder}
+						aboutPageImgList={aboutPageImgList}
+						aboutPageCaptionList={aboutPageCaptionList}
+						featurePageImgList={featurePageImgList}
+						featurePageCaptionList={featurePageCaptionList}
+						NFTPageImgList={NFTPageImgList}
+						NFTPageCaptionList={NFTPageCaptionList}
+						projectsPageImgList={projectsPageImgList}
+						projectsPageCaptionList={projectsPageCaptionList}
+					/>
+				</Suspense>
+			)}
+			<footer>
+				<h1>Get In Touch</h1>
+				<Suspense fallback={<p></p>}>
+					<SocialBubbles />
+				</Suspense>
+				<p>©2022 Kspaze1. All rights reserved.</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
--- kspaze1/src/components/PageAbout.js
+++ kspaze1/src/components/PageAbout.js
@@ -1,62 +1,162 @@
-export default function PageAbout({aboutPageImgList, aboutPageCaptionList,placeholder}) {
+export default function PageAbout({
+	aboutPageImgList,
+	aboutPageCaptionList,
+	placeholder,
+}) {
+	const successfulCaptionFetch = aboutPageCaptionList.length;
+	const placeholderCaption = "Artwork by Kspaze1";
 
-    const successfulCaptionFetch = aboutPageCaptionList.length
-    const placeholderCaption = 'Artwork by Kspaze1'
+	return (
+		<div>
+			<div className="panel">
+				<div className="text-container">
+					<div>
+						<h1>ABOUT</h1>
+					</div>
 
-    return (
-        <div>
-            <div className="panel" >
-            <div className="text-container">
-                    <div>
-                        <h1>ABOUT</h1>
-                    </div>
+					<div className="body-wrapper">
+						<div className="text-block">
+							<div className="image left">
+								<img
+									src={aboutPageImgList?.[0] || placeholder}
+									alt="me"
+									width="100%"
+									height="100%"
+								></img>
+								<div className="overlay">
+									<div className="img-credit">
+										{successfulCaptionFetch
+											? `${aboutPageCaptionList[0]}`
+											: placeholderCaption}
+									</div>
+								</div>
+							</div>
+							<h3>My traits</h3>
+							<ul>
+								<li>
+									BBC-featured tech founder, PhD in Chemistry
+									& Nanomaterials, Singapore’s EDGE
+									35-under-35.
+								</li>
+								<li>
+									Character artist with works featured by
+									eminent global musicians.
+								</li>
+								<li>GamestopNFT’s launch artist.</li>
+								<li>
+									Member of ARC Stellars – A curated
+									invite-only web3 community.
+								</li>
+								<li>
+									Bboy at roots – The Bronx Boys Rocking Crew,
+									Lords of Nanyang Crew, Flow with Life Crew,
+									Ill Stack Squad.
+								</li>
+							</ul>
+						</div>
 
-                <div className="body-wrapper">
-                        
-                    <div className='text-block'>
-                        <div className='image left'>
-                            <img src={aboutPageImgList?.[0] || placeholder} alt='me' width='100%' height='100%'></img>
-                            <div className="overlay">
-                                <div className="img-credit">{successfulCaptionFetch? `${aboutPageCaptionList[0]}` : placeholderCaption }</div>
-                            </div>
-                        </div>
-                        <h3>My traits</h3>
-                        <ul>
-                            <li>BBC-featured tech founder, PhD in Chemistry & Nanomaterials, Singapore’s EDGE 35-under-35.</li>
-                            <li>Character artist with works featured by eminent global musicians.</li>
-                            <li>GamestopNFT’s launch artist.</li>
-                            <li>Member of ARC Stellars – A curated invite-only web3 community.</li>
-                            <li>Bboy at roots – The Bronx Boys Rocking Crew, Lords of Nanyang Crew, Flow with Life Crew, Ill Stack Squad.</li>
-                        </ul>
-                        </div>
-                       
-                    
-
-                    <div className='text-block'>          
-                            <div className='image right' style={{padding:'3em 0 0 0'}}>
-                                <img src={aboutPageImgList?.[1] || placeholder} alt='me' width='100%' height='100%'></img>
-                                <div className="overlay">
-                                    <div className="img-credit">{successfulCaptionFetch? `${aboutPageCaptionList[1]}` : placeholderCaption }</div>
-                                </div>
-                            </div>
-                        <h3>My journey to web3</h3>
-                        <p>Born and raised on the streets of Saigon, Vietnam and spent the teenage years and adulthood in Singapore, I was fortunate to learn from the best in both environments. I learnt art at 5 years old and fell in love with the hip-hop culture through its art forms, namely hip-hop music, graffiti and breaking (bboying). 
-                            These street arts, and hip-hop rescued me from the darkest days as a troubled kid. I danced and played music so often that I skipped school. My pursuit in bboying has taught me various life lessons. I joined The Bronx Boys Rocking Crew – an global OG bboy crew originated from USA, and won many dance competitions in Singapore and the region with my greatest friends. </p>
-                        <p>The culture had also taught me the value of learning and contributing. I pursued a degree in Chemistry, and obtained a PhD in Chemistry from Nanyang Technological University, Singapore. I graduated with 2 Gold medals for Research Excellence and a patent, and I co-founded a deep-tech company to commercialize my research to combat COVID-19. The work has earned me Singapore’s EDGE 35-under-35 award and a feature on BBC and global media.</p>
-                        <p>However, it was tough to run a company and do art. My time with art was significantly affected. As COVID-19 subsided, I decided to pursue art again. </p>
-                        <p>This was also when I was invited as a launch artist for Gamestop NFT marketplace debut. I prepared the HITCats art collection and my proposal was approved by content team, alongside a licensed right to make a special 1/1 art with Gamestop Logo. I sold out the collection, amassing 24 ETH volume in 3 days. Subsequently, I did various collab NFT project with creators on Loopring Layer 2. </p>
-                        <p>Nevertheless, I am motivated to take a bigger step to push my arts further, and venture towards the 1/1 art scene. I am bullish on the utility of NFT that authenticates artworks and builds a digital version of traditional art collecting. I was humbled by many great artists in the space and realized upskilling is necessary. </p>
-                        </div>
-                    <div className='text-block'> 
-                     <h3>Art</h3>
-                            <p>I am in the early days of developing my style, which I aim to grow into something iconic in a few years. I was heavily influenced by graffiti arts and traditional American tattoo art styles – which emphasizes bold lines, rawness and a little bit of rebel. Started with doing art for fun without any real motivation about originality or finesse, I am learning to push my own boundaries towards creating more valuable arts.</p>
-
-                            <p>Instilling more thoughts in composition, colours, shadow play, story-telling while keeping the raw and rebellious essence of my street art roots is my goal. I am constantly working towards crafting more unique arts at the realm of mental activities and technologies – the areas that I am deeply passionate about.</p>
-              
-                </div>
-                </div>
-                </div>
-                </div>
-        </div>
-    );
+						<div className="text-block">
+							<div
+								className="image right"
+								style={{ padding: "3em 0 0 0" }}
+							>
+								<img
+									src={aboutPageImgList?.[1] || placeholder}
+									alt="me"
+									width="100%"
+									height="100%"
+								></img>
+								<div className="overlay">
+									<div className="img-credit">
+										{successfulCaptionFetch
+											? `${aboutPageCaptionList[1]}`
+											: placeholderCaption}
+									</div>
+								</div>
+							</div>
+							<h3>My journey to web3</h3>
+							<p>
+								I was fortunate to spend my years in a
+								combination of countries. I liked drawing since
+								young, but didn't practice it until I had my
+								first brush with the hip-hop culture in my
+								teenage years. I love the culture for its art
+								forms, namely hip-hop music, graffiti and
+								breaking (bboying). These street arts, and
+								hip-hop rescued me from the darkest days as a
+								troubled kid. I danced and played music so often
+								that I skipped school. My pursuit in bboying has
+								taught me various life lessons. I traveled
+								around to learn more about the art and competed
+								often alongside my greatest friends.
+							</p>
+							<p>
+								The culture had also taught me the value of
+								learning and contributing. I pursued a degree in
+								Chemistry, and obtained a PhD in Chemistry from
+								Nanyang Technological University, Singapore. I
+								graduated with a patent, and I co-founded a
+								company to commercialize my research to combat
+								COVID-19.
+							</p>
+							<p>
+								However, it was tough to run a COVID-19
+								operation during the pandemic and practice art.
+								My time with art was significantly affected. As
+								COVID-19 subsided, I decided to pursue art
+								again.
+							</p>
+							<p>
+								This was also when I was invited as a launch
+								artist for Gamestop NFT marketplace debut. I
+								prepared the HITCats art collection and my
+								proposal was approved by content team, alongside
+								a licensed right to make a special 1/1 art with
+								Gamestop Logo. I was glad to see the collection
+								- drawn with a theme of positivity (because I
+								pretty much needed it myself) - sold out on the
+								marketplace on the launch week with the total
+								volume of 21 ETH. Subsequently, I did various
+								collab NFT projects with creators on Loopring
+								Layer 2 ecosystem.
+							</p>
+							<p>
+								Nevertheless, I am motivated to take a bigger
+								step to push my arts further. I am bullish on
+								the utility of NFT that authenticates artworks
+								and builds a digital version of traditional art
+								collecting. I was humbled by many great artists
+								in the space and realized upskilling is
+								necessary.
+							</p>
+						</div>
+						<div className="text-block">
+							<h3>Art</h3>
+							<p>
+								I am in the early days of developing my style,
+								which I hope to grow into something iconic in a
+								few years. I was heavily influenced by graffiti
+								arts and traditional American tattoo art styles
+								– which emphasizes bold lines, rawness and a
+								little bit of rebel. Started with doing art for
+								fun without any real motivation about
+								originality or finesse, I am learning to push my
+								own boundaries towards creating more valuable
+								arts.
+							</p>
+							<p>
+								Instilling more thoughts in composition,
+								colours, shadow play, story-telling while
+								keeping the raw and rebellious essence of my
+								street art roots is my goal. I am constantly
+								working towards crafting more unique arts at the
+								realm of mental activities and technologies –
+								the areas that I am deeply passionate about.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
