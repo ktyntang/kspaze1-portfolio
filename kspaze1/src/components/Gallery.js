30fed6c4287216acf204df7076a61e3ed9fc9084
ktyntang <ktyntang@hotmail.com>
2022-12-11 20:42:55 +0800

    Updated projects gallery

--- kspaze1/src/components/Gallery.js
+++ kspaze1/src/components/Gallery.js
@@ -1,49 +1,74 @@
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import { useState} from "react";
+import { useState } from "react";
 import "./Gallery.css";
 
-export default function Gallery({images,captions}) {
-const [selectedImg, setSelectedImg] = useState(null);
-   const [zoomFill, setZoomFill] = useState('zoomFillIn');
-   
-  const closeImg =() =>{
-    setZoomFill('zoomFillOut')
-  }
-   const openImg = (image,i)=> {
-    setSelectedImg({image:image, index:i})
-   }
+export default function Gallery({ images, captions }) {
+	const [selectedImg, setSelectedImg] = useState(null);
+	const [zoomFill, setZoomFill] = useState("zoomFillIn");
 
-   const resetImg = () =>{
-    setSelectedImg(null)
-    setZoomFill('zoomFillIn')
-   }
-   
-   return (
-        <div className="scroll-container">
-            <div className="gallery-container">
-                    <ResponsiveMasonry
-                        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-                        <Masonry columnsCount={3} gutter="10px">
-                        {images.map((image,i) => {
-                            return (
-                            <div className="image-container" onClick={()=>openImg(image,i)} key={`${image.split('token=')[1]}`} >
-                                <img src={image} alt={`${image}`}></img>
-                                <div className="overlay">
-                                </div>
-                            </div>)
-                        })}
-                        </Masonry>
-                    </ResponsiveMasonry>
-                    
-            </div>
+	const closeImg = () => {
+		setZoomFill("zoomFillOut");
+	};
+	const openImg = (image, i) => {
+		setSelectedImg({ image: image, index: i });
+	};
 
-            <div className="selected-image-container" 
-            style={{opacity:`${selectedImg? '1':'0'}`,pointerEvents:`${selectedImg? 'visible':'none'}` }}>
-                        <div className="selected-image" style={{display:`${selectedImg? 'block':'none'}`}} animation={zoomFill} onAnimationEnd={()=>{if(zoomFill==='zoomFillOut')resetImg()}} onClick={()=>closeImg()}>
-                            <img src={selectedImg?selectedImg.image:null} alt={selectedImg?`${selectedImg.image}`:null} ></img>
-                            <div className="img-credit">{selectedImg?`${captions[selectedImg.index]}`:null}</div>
-                        </div>
-                    </div>
-        </div>
-    );
+	const resetImg = () => {
+		setSelectedImg(null);
+		setZoomFill("zoomFillIn");
+	};
+
+	return (
+		<div className="scroll-container">
+			<div className="gallery-container">
+				<ResponsiveMasonry
+					columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+				>
+					<Masonry columnsCount={3} gutter="10px">
+						{images.map((image, i) => {
+							return (
+								<div
+									className="image-container"
+									onClick={() => openImg(image, i)}
+									key={`${image.split("token=")[1]}`}
+								>
+									<img src={image} alt={`${image}`}></img>
+									<div className="overlay"></div>
+								</div>
+							);
+						})}
+					</Masonry>
+				</ResponsiveMasonry>
+			</div>
+
+			<div
+				className="selected-image-container"
+				style={{
+					opacity: `${selectedImg ? "1" : "0"}`,
+					pointerEvents: `${selectedImg ? "visible" : "none"}`,
+				}}
+			>
+				<div
+					className="selected-image"
+					style={{ display: `${selectedImg ? "block" : "none"}` }}
+					animation={zoomFill}
+					onAnimationEnd={() => {
+						if (zoomFill === "zoomFillOut") resetImg();
+					}}
+					onClick={() => closeImg()}
+				>
+					<img
+						src={selectedImg ? selectedImg.image : null}
+						alt={selectedImg ? `${selectedImg.image}` : null}
+					></img>
+					{/* PageProjects has different caption nesting from other pages */}
+					<div className="img-credit">
+						{selectedImg
+							? `${captions[selectedImg.index].caption}`
+							: null}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- kspaze1/src/components/PageProjects.js
+++ kspaze1/src/components/PageProjects.js
@@ -1,42 +1,49 @@
-import Gallery from './Gallery';
+import Gallery from "./Gallery";
 
-export default function PageProjects({projectsPageImgList,projectsPageCaptionList, placeholder}) {
-    
-    const successfulImgFetch = projectsPageImgList.length
-    const placeholderCaption = 'Artwork by Kspaze1'
-    
-    return (
-        <div id='pageProjects'>
-        <div className="panel">
-                <div className="text-container">
-                    <div >
-                        <h1>PROJECTS</h1>
-                    </div>
-                    <div className="section">
-                    <div className="text left ">
-                        <div className='text-container'>
-                            <div>
-                                <h3>My past web2 projects</h3>
-                            </div>
-                            <div className="body-wrapper">
-                                <ul>
-                                    <li>Music cover</li>
-                                    <li>Character arts</li>
-                                    <li>Branding </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
+export default function PageProjects({
+	projectsPageImgList,
+	projectsPageCaptionList,
+	placeholder,
+}) {
+	const successfulImgFetch = projectsPageImgList.length;
+	const placeholderCaption = "Artwork by Kspaze1";
 
-                </div>
-                        </div>
-                    <div className='gallery'>
-                        {successfulImgFetch ? 
-                        <Gallery images={projectsPageImgList} captions={projectsPageCaptionList}/> 
-                        : <img src={placeholder} alt={placeholderCaption}></img>
-                    }
-                    </div>
-            </div>
-    );
+	return (
+		<div id="pageProjects">
+			<div className="panel">
+				<div className="text-container">
+					<div>
+						<h1>PROJECTS</h1>
+					</div>
+					<div className="section">
+						<div className="text left ">
+							<div className="text-container">
+								<div>
+									<h3>My past web2 projects</h3>
+								</div>
+								<div className="body-wrapper">
+									<ul>
+										<li>Music cover</li>
+										<li>Character arts</li>
+										<li>Branding </li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* PageProjects is updated newest at top. e.g. img 71 shown before img 70 */}
+			<div className="gallery">
+				{successfulImgFetch ? (
+					<Gallery
+						images={projectsPageImgList.slice().reverse()}
+						captions={projectsPageCaptionList.slice().reverse()}
+					/>
+				) : (
+					<img src={placeholder} alt={placeholderCaption}></img>
+				)}
+			</div>
+		</div>
+	);
 }
